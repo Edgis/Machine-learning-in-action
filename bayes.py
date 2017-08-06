154c4190ada4bf833e6ab0b72a3c24881ed1541f
Edgis <1398107093@qq.com>
2017-08-06 21:47:10 +0800

    Update bayes.py
    edition:python3.5
    spamTest()中在python3中的错误以勘误，可直接运行

--- bayes.py
+++ bayes.py
@@ -57,8 +57,48 @@ def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
     else:
         return  0
 
+def textParse(bigString):
+    import re
+    listOfTokens = re.split(r'\W*', bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
 
+def spamTest():
+    docList = []
+    classList = []
+    fullText = []
+    for i in range(1, 26):
+        wordList = textParse(open('email/spam/%d.txt'%i, encoding='gbk', errors='ignore').read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList = textParse(open('email/ham/%d.txt'%i, encoding='gbk', errors='ignore').read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    vocabList = createVocabList(docList)
+    trainingSet = list(range(50))#trainingSet = range(50)#
+    testSet = []
+    #随机构建训练集
+    for i in range(10):
+        randIndex = int(random.randint(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+    trainMat = []
+    trainClasses = []
+    for docIndex in trainingSet:
+        trainMat.append(setOfWords2Vec(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V, p1V, pSpam = trainNBO(array(trainMat), array(trainClasses))
+    errorCount = 0
+    #对测试集进行分类
+    for docIndex in testSet:
+        wordVector = setOfWords2Vec(vocabList, docList[docIndex])
+        if classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+            errorCount += 1
+    print('the error rate is :', float(errorCount)/len(testSet))
+    
 if __name__ == "__main__":
+    spamTest()
     '''
      listOPosts, listClasses = loadDataSet()
     myVocabList = createVocabList(listOPosts)
